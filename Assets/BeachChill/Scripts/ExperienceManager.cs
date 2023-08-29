cc8d1c63c8a0c925d546e31c2dc5fd98a20ac072
Anton-Yo <79824285+Anton-Yo@users.noreply.github.com>
2023-08-29 18:26:56 +1000

    Sun/Experience manager created. TMP imported
    Added textmeshpro and also the sun has tunable speed values.
    1. can be set to go off manual movespeed
    2. can be auto calculated according to experience length (plus some buffer to show it works rn)

---
+++ Assets/BeachChill/Scripts/ExperienceManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExperienceManager : MonoBehaviour
+{
+
+    [SerializeField] private float experienceLength;
+    public float ExperienceLength
+    {
+        get { return experienceLength;}
+    }
+
+    private float experienceTimer;
+
+    static private ExperienceManager instance;
+    static public ExperienceManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                Debug.LogError("There is no GameManager in the scene.");
+            }
+            return instance;
+        }
+    }
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    [SerializeField] private GameObject fadeToBlack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        experienceTimer = experienceLength;
+
+        if(fadeToBlack.activeSelf)
+        {
+            fadeToBlack.SetActive(false);
+        }
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        experienceTimer -= Time.deltaTime;
+
+        if(experienceTimer < 0)
+        {
+            fadeToBlack.SetActive(true);
+        }
+    }
+
+}
--- Assets/BeachChill/Scripts/SunMovement.cs
+++ Assets/BeachChill/Scripts/SunMovement.cs
@@ -5,51 +5,73 @@ using UnityEngine;
 public class SunMovement : MonoBehaviour
 {
 
+    [Header("Necessary GameObjects")]
+    [SerializeField] private GameObject sun;
     [SerializeField] private GameObject startPoint;
     [SerializeField] private GameObject endPoint;
+ 
+    [Header("MoveSpeed related")]
+    [SerializeField] private float movementSpeed = 10;
+    private enum sunSpeedBasis
+    {
+        MoveSpeed,
+        OverallTime
+    }
+    [SerializeField] private sunSpeedBasis SunSpeedBasis;
+    [SerializeField] private  float timeBuffer = 1;
 
-    [SerializeField] private float movementSpeed;
-    private Vector3 moveDir = new Vector3(0,0,0);
+    private Vector3 currentSunPos = new Vector3(0,0,0);
+
+    private Vector3 targetPos = new Vector3(0,0,0);
+    
+    private Vector3 movementPerFrame = new Vector3(0,0,0);
 
 
     // Start is called before the first frame update
     void Start()
     {
-        if(startPoint == null || endPoint == null) // null check for necessary start/end points
-        {
-            startPoint = this.gameObject.transform.parent.gameObject;
-            endPoint = startPoint.transform.GetChild(1).gameObject;
+        if(startPoint == null || endPoint == null || sun == null) // null check for start/end points
+        {   
+            sun = transform.GetChild(0).gameObject;
+            startPoint = transform.GetChild(1).gameObject;
+            endPoint = transform.GetChild(2).gameObject;
         }
 
-        moveDir = startPoint.transform.position - endPoint.transform.position; //grabs direction the sun must move to go from Point A -> B
-        moveDir = moveDir.normalized;
+        sun.transform.position = startPoint.transform.position; // make sure sun is at startPoint. It should be by default but hey you never know
+        targetPos = endPoint.transform.position;
+
+        switch(SunSpeedBasis) //if want to use movement speed use case 0. If want to have sun speed calculated based on overall experience time, then pick case 1;
+        {
+            case(sunSpeedBasis.MoveSpeed): //movementSpeed is unaffected
+            
+            break;
+
+            case(sunSpeedBasis.OverallTime):
+                movementSpeed = Vector3.Distance(startPoint.transform.position, endPoint.transform.position)/(ExperienceManager.Instance.ExperienceLength + timeBuffer); //calc movement speed of sun if it is to reach the endPoint by end of experience
+            break;
+        }
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if(transform.localPosition.z > endPoint.transform.localPosition.z) //While the sun is not at the destination, move it towards the destination at a set speed depending on if +- z dir
-        {
-            transform.localPosition = transform.localPosition - moveDir * movementSpeed * Time.deltaTime;
-
-            if(transform.localPosition.z < endPoint.transform.localPosition.z) //if passed end point, set transform.position to end point.
+        currentSunPos = sun.transform.position; //grab the transform.position value once cos its easier
+       
+            if(currentSunPos.y > targetPos.y)
             {
-                transform.localPosition = endPoint.transform.localPosition;
+                sun.transform.position = Vector3.MoveTowards(sun.transform.position, endPoint.transform.position, movementSpeed * Time.deltaTime);
             }
-        }
 
-        if(transform.localPosition.z < endPoint.transform.localPosition.z) //While the sun is not at the destination, move it towards the destination at a set speed depending on if +- z dir
-        {
-            transform.localPosition = transform.localPosition + moveDir * movementSpeed * Time.deltaTime;
 
-            if(transform.localPosition.z > endPoint.transform.localPosition.z) // if passed end point, set transform.position to end point.
+            if(currentSunPos.y <= targetPos.y) //check if it will have gone past the end point
             {
-                transform.localPosition = endPoint.transform.localPosition;
+                currentSunPos = targetPos;
+                Debug.Log("Sun has set and has turned to red");
+                sun.GetComponent<Renderer>().material.color = Color.red;
             }
+      
         }
-        
-        
-
-    }
+    
 }
