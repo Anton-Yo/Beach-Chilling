35d57dbfef08556d4151ae25a8ca0df94a7c84c0
calebku1 <caleb.ku@students.mq.edu.au>
2023-09-30 18:58:00 +1000

    BreathingCircleV2

--- Assets/Materials/CircleMaterial/VisualCircleMove.cs
+++ Assets/Materials/CircleMaterial/VisualCircleMove.cs
@@ -9,15 +9,26 @@ public class VisualCircleMove : MonoBehaviour
     [SerializeField] private Image circleIndicator;
     [SerializeField] private float circleIndicatorSpeed;
     [SerializeField] private RectTransform button;
+    [SerializeField] private float delayStartTimer;
     private float visualTimer = 0;
     private bool circleFilled = false;
     private float buttonTimer = 0;
+    private float buttonStartLocation;
+
+    void Start()
+    {
+        buttonStartLocation = button.localEulerAngles.z;
+        Debug.Log(buttonStartLocation);
+    }
     
     void Update()
     {
-        TimerSetup();
-        
-        CircleChange(visualTimer);
+        delayStartTimer-=Time.deltaTime;
+        if(delayStartTimer<=0)
+        {
+            TimerSetup();
+            CircleChange(visualTimer);
+        }
     }
 
     void CircleChange(float circleTimer)
@@ -37,7 +48,7 @@ public class VisualCircleMove : MonoBehaviour
         //button setup
         float buttonAmount = buttonTimer*circleIndicatorSpeed/360;
         float buttonAngle = buttonAmount*360;
-        button.localEulerAngles = new Vector3(0,0,-buttonAngle);
+        button.localEulerAngles = new Vector3(0,0,-buttonAngle+buttonStartLocation);
         
     }
 
