ddb0ce4238a7c5eea0afc7f652b98d2c6f05b826
calebku1 <caleb.ku@students.mq.edu.au>
2023-10-12 23:25:25 +1100

    No Delay Sun

---
+++ Assets/BeachChillAssets/Scripts/SunStart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunStart : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem system;
+
+    void Start()
+    {
+        system.Simulate(10, true);
+        system.Play(true);
+    }
+
+}
