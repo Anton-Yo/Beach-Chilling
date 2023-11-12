7aba6a7aecde47d4ec29076273097497aae6268f
calebku1 <caleb.ku@students.mq.edu.au>
2023-11-12 23:17:40 +1100

    added text aid and fade in effect to the circle

--- Assets/Scripts/VisualCircleMove.cs
+++ Assets/Scripts/VisualCircleMove.cs
@@ -10,16 +10,26 @@ public class VisualCircleMove : MonoBehaviour
     [SerializeField] private float circleIndicatorSpeed;
     [SerializeField] private RectTransform button;
     [SerializeField] private float delayStartTimer;
-    private float visualTimer = 0;
     [SerializeField] private bool pauseTimer;
+    [SerializeField] private CanvasGroup circleGroup;
+    [SerializeField] private CanvasGroup inText;
+    [SerializeField] private CanvasGroup outText;
+    [SerializeField] private float fadeInTime;
+
+    private float visualTimer = 0;
     private bool circleFilled = false;
     private float buttonTimer = 0;
     private float buttonStartLocation;
+    private bool fadeIn = true;
+    private bool inTextFadeIn = false;
+    private bool outTextFadeOut = false;
+    private float outTextFadeOutSpeed = 0.2f;
 
     void Start()
     {
         buttonStartLocation = button.localEulerAngles.z;
         Debug.Log(buttonStartLocation);
+
     }
     
     void Update()
@@ -31,7 +41,13 @@ public class VisualCircleMove : MonoBehaviour
             {
                 TimerSetup();
                 CircleChange(visualTimer);
+                TextChange();
             }
+        }
+        
+        if(fadeIn)
+        {
+            circleGroup.alpha+= fadeInTime*Time.deltaTime;
         }
     }
 
@@ -83,5 +99,46 @@ public class VisualCircleMove : MonoBehaviour
         }
 
         buttonTimer+= Time.deltaTime;
+    }
+
+    void TextChange()
+    {
+        if(circleIndicator.fillAmount <= 0 || circleIndicator.fillAmount >= 1)
+        {
+            outTextFadeOut = false;
+            outText.alpha = 0;
+            inTextFadeIn = true;
+        }
+
+        if(circleIndicator.fillAmount >= 0.3 && circleIndicator.fillClockwise == true)
+        {
+            outTextSetup();
+        }
+
+        if(circleIndicator.fillAmount <= 0.7 && circleIndicator.fillClockwise == false)
+        {
+            outTextSetup();
+        }
+
+
+        if(inTextFadeIn)
+        {
+            inText.alpha+=Time.deltaTime;
+        }
+        if(outTextFadeOut)
+        {
+            outText.alpha-=Time.deltaTime*outTextFadeOutSpeed;
+        }
+    }
+
+    void outTextSetup()
+    {
+        if(outText.alpha <=0 && inTextFadeIn == true)
+        {
+            outText.alpha = 1;
+        }
+        inTextFadeIn = false;
+        inText.alpha = 0;
+        outTextFadeOut = true;
     }
 }
